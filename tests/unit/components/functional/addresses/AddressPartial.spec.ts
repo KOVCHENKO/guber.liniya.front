75e4f263d231025f9a4d34450712fbcf64106459
il-kow@mail.ru <il-kow@mail.ru>
2019-01-16 18:55:46 +0300

    feat(test) AddressPartial spec has been appended

---
+++ tests/unit/components/functional/addresses/AddressPartial.spec.ts
@@ -0,0 +1,14 @@
+import { shallowMount } from '@vue/test-utils'
+import AddressPartial from '@/components/functional/addresses/AddressPartial.vue'
+
+describe('AddressPartial.vue', () => {
+    it('includes addresses when method is called', () => {
+        const msg = 'new message'
+
+        const wrapper = shallowMount(AddressPartial, {
+            propsData: { msg }
+        })
+
+        expect(wrapper.text()).toMatch(msg)
+    })
+})
