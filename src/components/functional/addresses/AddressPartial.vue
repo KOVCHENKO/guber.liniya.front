55e3c38780121773dc094e226cfaa3ada4051748
ILKOV <il-kow@mail.ru>
2019-02-19 18:53:14 +0300

    upd(AddressPartial) for queries to new FIAS service

--- src/components/functional/addresses/AddressPartial.vue
+++ src/components/functional/addresses/AddressPartial.vue
@@ -6,7 +6,7 @@
             <div class="col-sm-5 clearfix">
                 <label for="claim_city">Город</label>
                 <input :name="$t('validation.address')" id="claim_city" v-model="search.city"
-                              @keydown="changeCity" @click="showCitiesList" autocomplete="off"/>
+                       @keydown="changeCity" @click="showCitiesList" autocomplete="off"/>
 
                 <vue-scrollbar class="menu-scrollbar dropdown-menu dropdown-menu-right" ref="scrollbar"
                                v-show="visibility.cityList">
@@ -23,7 +23,7 @@
             <div class="col-sm-5 clearfix">
                 <label for="claim_city">Улица</label>
                 <input :name="$t('validation.address')" id="claim_street" v-model="search.street" ref="street"
-                              @keydown="changeStreet" @click="showStreetsList" autocomplete="off"/>
+                       @keydown="changeStreet" @click="showStreetsList" autocomplete="off"/>
 
                 <vue-scrollbar class="menu-scrollbar dropdown-menu dropdown-menu-right" ref="scrollbar"
                                v-show="visibility.streetList">
@@ -38,8 +38,8 @@
             <!-- Дом -->
             <div class="col-sm-2 clearfix">
                 <label for="claim_house">Дом</label>
-                <input :name="$t('validation.address')" id="claim_house" v-model="search.building"
-                              @keydown="changeBuilding" autocomplete="off"/>
+                <input :name="$t('validation.address')" id="claim_house" v-model="search.building" ref="building"
+                       @keydown="changeBuilding" autocomplete="off"/>
 
                 <vue-scrollbar class="menu-scrollbar dropdown-menu dropdown-menu-right" ref="scrollbar"
                                v-show="visibility.buildingList">
@@ -57,180 +57,187 @@
 </template>
 
 <script>
-import axios from 'axios';
-import {fiasUrl} from '@/globals';
-import ScrollBar from 'vue2-scrollbar'
+    import axios from 'axios';
+    import {fiasUrl} from '@/globals';
+    import ScrollBar from 'vue2-scrollbar'
+    import {fiasToken} from "../../../globals";
 
-export default {
-    name: 'AddressPartial',
+    export default {
+        name: 'AddressPartial',
 
-    components: {
-        VueScrollbar: ScrollBar
-    },
-
-    props: {
-        validation: String,
-        city: String,
-        street: String,
-        building: String,
-    },
-
-    data() {
-
-        return {
-            //Списки всех городов, улиц и домов. Загружаются с FIAS по API
-            cities: [],
-            streets: [],
-            buildings: [],
-
-            //Видимость выпадающих списков
-            visibility: {cityList: false, streetList: false, buildingList: false,},
-
-            //Выбранные текущие данные (объект)
-            current: {city: {}, street: {}, building: {}, shortcadnum: ''},
-
-            //Строка поиска по спискам городов, улиц, домов, короткий кадастровый номер
-            search: {city: '', street: '', building: '', shortcadnum: ''}
-        }
-    },
-
-    computed: {
-        /* Проверка ввода адресной информации */
-        addressValidation() {
-            if (this.validation['address'] === undefined) return {};
-            return this.validation['address'];
+        components: {
+            VueScrollbar: ScrollBar
         },
 
-        /* Отфильтрованный список городов */
-        filteredCities() {
-            let search = this.search.city === undefined ? '' : this.search.city;
-            return this.cities.filter(item => {
-                let city = item.SHORTNAME + '. ' + item.FORMALNAME;
-                if (city.toLowerCase().indexOf(search.toLowerCase()) !== -1) return item;
-            });
+        props: {
+            validation: String,
+            city: String,
+            street: String,
+            building: String,
         },
 
-        /* Отфильтрованный список улиц */
-        filteredStreets() {
-            let search = this.search.street === undefined ? '' : this.search.street;
-            return this.streets.filter(item => {
-                let street = item.FORMALNAME;
-                if (street.toLowerCase().indexOf(search.toLowerCase()) !== -1) return item;
-            });
+        data() {
+
+            return {
+                //Списки всех городов, улиц и домов. Загружаются с FIAS по API
+                cities: [],
+                streets: [],
+                buildings: [],
+
+                //Видимость выпадающих списков
+                visibility: {cityList: false, streetList: false, buildingList: false,},
+
+                //Выбранные текущие данные (объект)
+                current: {city: {}, street: {}, building: {}, shortcadnum: ''},
+
+                //Строка поиска по спискам городов, улиц, домов, короткий кадастровый номер
+                search: {city: '', street: '', building: '', shortcadnum: ''}
+            }
         },
 
-        /* Отфильтрованный список домов */
-        filteredBuildings() {
-            let search = this.search.building === undefined ? '' : this.search.building;
-            return this.buildings.filter(item => {
-                let building = item.FORMALNAME;
-                if (building.toLowerCase().indexOf(search.toLowerCase()) !== -1) return item;
-            });
-        },
-    },
+        computed: {
+            /* Проверка ввода адресной информации */
+            addressValidation() {
+                if (this.validation['address'] === undefined) return {};
+                return this.validation['address'];
+            },
 
-    methods: {
+            /* Отфильтрованный список городов */
+            filteredCities() {
+                let search = this.search.city === undefined ? '' : this.search.city;
+                return this.cities.filter(item => {
+                    let city = item.SHORTNAME + '. ' + item.FORMALNAME;
+                    if (city.toLowerCase().indexOf(search.toLowerCase()) !== -1) return item;
+                });
+            },
 
-        /* Получить список городов */
-        getCitiesList() {
-            axios.get(fiasUrl + 'choose_cities').then(response => {
-                this.cities = response.data;
-            });
-        },
+            /* Отфильтрованный список улиц */
+            filteredStreets() {
+                let search = this.search.street === undefined ? '' : this.search.street;
+                return this.streets.filter(item => {
+                    let street = item.FORMALNAME;
+                    if (street.toLowerCase().indexOf(search.toLowerCase()) !== -1) return item;
+                });
+            },
 
-        /* Отобразить выпадающий список с городами */
-        showCitiesList() {
-            this.visibility.cityList = true;
-            this.visibility.streetList = false;
-            this.visibility.buildingList = false;
-        },
-
-        /* Событие: начали ввод символов в поле для выбора города */
-        changeCity() {
-            this.showCitiesList();
-            this.current.street = {};
-            this.current.building = {};
-        },
-
-        /* Выбрали новый город */
-        setCity(city) {
-            this.search.city = city.SHORTNAME + '. ' + city.FORMALNAME;
-            this.visibility.cityList = false;
-            this.current.city = city;
-
-            this.$emit('update:city', city.SHORTNAME + '. ' + city.FORMALNAME);
-
-            this.getStreetsList();
+            /* Отфильтрованный список домов */
+            filteredBuildings() {
+                let search = this.search.building === undefined ? '' : this.search.building;
+                return this.buildings.filter(item => {
+                    let building = item.FORMALNAME;
+                    if (building.toLowerCase().indexOf(search.toLowerCase()) !== -1) return item;
+                });
+            },
         },
 
-        /* Получить список улиц */
-        getStreetsList() {
-            axios.get(fiasUrl + 'choose_street/' + this.current.city.AOGUID).then(response => {
-                this.streets = response.data;
+        methods: {
+
+            /* Получить список городов */
+            getCitiesList() {
+                axios.get(fiasUrl + 'choose_cities?token=' + fiasToken,  {
+                    transformRequest(data, headers) { delete headers.common.Authorization; return data; },
+                }).then(response => {
+                    this.cities = response.data;
+                });
+            },
+
+            /* Отобразить выпадающий список с городами */
+            showCitiesList() {
+                this.visibility.cityList = true;
+                this.visibility.streetList = false;
+                this.visibility.buildingList = false;
+            },
+
+            /* Событие: начали ввод символов в поле для выбора города */
+            changeCity() {
+                this.showCitiesList();
+                this.current.street = {};
+                this.current.building = {};
+            },
+
+            /* Выбрали новый город */
+            setCity(city) {
+                this.search.city = city.SHORTNAME + '. ' + city.FORMALNAME;
+                this.visibility.cityList = false;
+                this.current.city = city;
+
+                this.$emit('update:city', city.SHORTNAME + '. ' + city.FORMALNAME);
+
+                this.getStreetsList();
+            },
+
+            /* Получить список улиц */
+            getStreetsList() {
+                axios.get(fiasUrl + 'choose_streets/' + this.current.city.AOGUID + '?token=' + fiasToken,  {
+                    transformRequest(data, headers) { delete headers.common.Authorization; return data; },
+                }).then(response => {
+                    this.streets = response.data;
+                    this.showStreetsList();
+                });
+            },
+
+            /* Отобразить выпадающий список с улицами */
+            showStreetsList() {
+                this.visibility.cityList = false;
+                this.visibility.streetList = true;
+                this.visibility.buildingList = false;
+                this.$refs.street.focus();
+            },
+
+            /* Событие: начали ввод символов в поле для выбора улицы */
+            changeStreet() {
                 this.showStreetsList();
-            });
-        },
+                this.current.building = {};
+            },
 
-        /* Отобразить выпадающий список с улицами */
-        showStreetsList() {
-            this.visibility.cityList = false;
-            this.visibility.streetList = true;
-            this.visibility.buildingList = false;
-            this.$refs.street.focus();
-        },
+            /* Выбрали новую улицу */
+            setStreet(street) {
+                this.search.street = street.FORMALNAME;
+                this.current.street = street;
 
-        /* Событие: начали ввод символов в поле для выбора улицы */
-        changeStreet() {
-            this.showStreetsList();
-            this.current.building = {};
-        },
+                this.$emit('update:street', street.FORMALNAME);
 
-        /* Выбрали новую улицу */
-        setStreet(street) {
-            this.search.street = street.FORMALNAME;
-            this.current.street = street;
+                this.getBuildingsList();
+            },
 
-            this.$emit('update:street', street.FORMALNAME);
+            /* Получить список домов */
+            getBuildingsList() {
+                axios.get(fiasUrl + 'choose_buildings/' + this.current.street.AOGUID + '?token=' + fiasToken,  {
+                    transformRequest(data, headers) { delete headers.common.Authorization; return data; },
+                }).then(response => {
+                    this.buildings = response.data;
+                    this.showBuildingsList();
+                });
+            },
 
-            this.getBuildingsList();
-        },
+            /* Отобразить выпадающий список с домами */
+            showBuildingsList() {
+                this.visibility.cityList = false;
+                this.visibility.streetList = false;
+                this.visibility.buildingList = true;
+                this.$refs.building.focus();
+            },
 
-        /* Получить список домов */
-        getBuildingsList() {
-            axios.get(fiasUrl + 'choose_building/' + this.current.street.AOGUID).then(response => {
-                this.buildings = response.data;
+            /* Событие: начали ввод символов в поле для выбора дома */
+            changeBuilding() {
                 this.showBuildingsList();
-            });
+            },
+
+            /* Выбрали новый дом */
+            setBuilding(building) {
+                this.current.building = building;
+                this.search.building = building.FORMALNAME;
+                this.visibility.buildingList = false;
+
+                this.$emit('update:building', building.FORMALNAME);
+            },
+
         },
 
-        /* Отобразить выпадающий список с домами */
-        showBuildingsList() {
-            this.visibility.cityList = false;
-            this.visibility.streetList = false;
-            this.visibility.buildingList = true;
-            this.$refs.building.focus();
-        },
+        created() {
+            let a = this.value;
+            this.getCitiesList();
+        }
 
-        /* Событие: начали ввод символов в поле для выбора дома */
-        changeBuilding() {
-            this.showBuildingsList();
-        },
-
-        /* Выбрали новый дом */
-        setBuilding(building) {
-            this.current.building = building;
-            this.search.building = building.FORMALNAME;
-            this.visibility.buildingList = false;
-
-            this.$emit('update:building', building.FORMALNAME);
-        },
-
-    },
-
-    created() {
-        let a = this.value;
-        this.getCitiesList();
     }
-
-}
 </script>
--- src/globals.ts
+++ src/globals.ts
@@ -5,3 +5,4 @@ export const baseUrl = 'http://guber.liniya.back/api/';
 export const baseRootUrl = 'http://guber.liniya.back/';
 
 export const fiasUrl = 'https://fias.dev-io.ru/';
+export const fiasToken = 'c53aa8a7f713fddeaaacd82560842984';
