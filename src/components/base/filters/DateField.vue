51d3f37a0a62de66dc10c5917fe8cd6f98a5b5c4
ILKOV <il-kow@mail.ru>
2019-03-04 11:09:45 +0300

    upd(DateField) get rid of commented code

--- src/components/base/filters/DateField.vue
+++ src/components/base/filters/DateField.vue
@@ -32,28 +32,5 @@
             this.search({ string: this.dataFilterString, field: 'date'});
         }
 
-
-
-
-        // get rid of
-        //
-        // @Prop() public dataFilter: any;
-        //
-        // @Provide() public minDate = this.dataFilter.minDate;
-        // @Provide() public maxDate = this.dataFilter.maxDate;
-        //
-        // @State('pagination') public paginationState!: IPaginationState;
-        //
-        // @Action('getAllClaimsOfOrganization2')
-        // public getAllClaimsOfOrganization2;
-        //
-        // public startSearch() {
-        //     this.paginationState.currentPage = 1;
-        //
-        //     this.dataFilter.minDate = (this.minDate) ? moment(this.minDate).format('YYYY-MM-DD hh:mm:ss') : '';
-        //     this.dataFilter.maxDate = (this.maxDate) ? moment(this.maxDate).format('YYYY-MM-DD hh:mm:ss') : '';
-        //     this.getAllClaimsOfOrganization2(this.dataFilter);
-        // }
-
     }
 </script>
